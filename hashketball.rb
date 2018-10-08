e5cf2d91119737f54ff8a07996888c066d77a399
vibhu mahendru <vibhumahendru@gmail.com>
2018-10-08 22:49:38 +0000

    automatically backed up by learn

--- hashketball.rb
+++ hashketball.rb
@@ -1,132 +1,144 @@
+game_hash = {
+  :home => {
+    :team_name => "Monstars",
+    :colors => ["black", "green"],
+    :players => {
+      
+      "Pound" => {
+        :number => 99,
+        :shoe_size => 10,
+        :stats => {
+          :points => 40,
+          :rebounds => 11,
+          :assists => 12,
+          :steals => 8,
+          :blocks => 9,
+          :slam_dunks => 6
+        }
+      },
 
+      "Bupkus" => {
+        :number => 95,
+        :shoe_size => 11,
+        :stats => {
+          :points => 32,
+          :rebounds => 16,
+          :assists => 8,
+          :steals => 12,
+          :blocks => 15,
+          :slam_dunks => 8
+        }
+      },
 
+      "Bang" => {
+        :number => 93,
+        :shoe_size => 12,
+        :stats => {
+          :points => 24,
+          :rebounds => 9,
+          :assists => 13,
+          :steals => 10,
+          :blocks => 6,
+          :slam_dunks => 3
+        }
+      },
 
-def game_hash
-  {
-  :home_team => {
-    :name => "Brooklyn Nets",
-    :colors => ["Black", "White"],
-    :players => [
-      {
-      :name => "Alan Anderson",
-      :number => 0,
-      :shoe => 16,
-      :points => 22,
-      :rebounds => 12,
-      :assists => 12,
-      :steals => 3,
-      :blocks => 1,
-      :slam_dunks => 1
+      "Blanko" => {
+        :number => 88,
+        :shoe_size => 14,
+        :stats => {
+          :points => 10,
+          :rebounds => 12,
+          :assists => 8,
+          :steals => 6,
+          :blocks => 4,
+          :slam_dunks => 0
+        }
       },
-      {
-      :name => "Reggie Evans",
-      :number => 30,
-      :shoe => 14,
-      :points => 12,
-      :rebounds => 12,
-      :assists => 12,
-      :steals => 12,
-      :blocks => 12,
-      :slam_dunks => 7
+
+      "Nawt" => {
+        :number => 85,
+        :shoe_size => 13,
+        :stats => {
+          :points => 18,
+          :rebounds => 4,
+          :assists => 10,
+          :steals => 3,
+          :blocks => 9,
+          :slam_dunks => 7
+        }
       },
-      {
-      :name => "Brook Lopez",
-      :number => 11,
-      :shoe => 17,
-      :points => 17,
-      :rebounds => 19,
-      :assists => 10,
-      :steals => 3,
-      :blocks => 1,
-      :slam_dunks => 15
-      },
-      {
-      :name => "Mason Plumlee",
-      :number => 1,
-      :shoe => 19,
-      :points => 26,
-      :rebounds => 12,
-      :assists => 6,
-      :steals => 3,
-      :blocks => 8,
-      :slam_dunks => 5
-      },
-      {
-      :name => "Jason Terry",
-      :number => 31,
-      :shoe => 15,
-      :points => 19,
-      :rebounds => 2,
-      :assists => 2,
-      :steals => 4,
-      :blocks => 11,
-      :slam_dunks => 1
-      }
-    ]
+    },
   },
-  :away_team => {
-    :name => "Charlotte Hornets",
-    :colors => ["Turquoise", "Purple"],
-    :players => [
-      {
-      :name => "Jeff Adrien",
-      :number => 4,
-      :shoe => 18,
-      :points => 10,
-      :rebounds => 1,
-      :assists => 1,
-      :steals => 2,
-      :blocks => 7,
-      :slam_dunks => 2
+  :away => {
+    :team_name => "Toon Squad",
+    :colors => ["white", "blue"],
+    :players => {
+      
+      "Bugs" => {
+        :number => 1,
+        :shoe_size => 10,
+        :stats => {
+          :points => 30,
+          :rebounds => 13,
+          :assists => 10,
+          :steals => 10,
+          :blocks => 8,
+          :slam_dunks => 5
+        }
       },
-      {
-      :name => "Bismak Biyombo", 
-      :number => 0,
-      :shoe => 16,
-      :points => 12,
-      :rebounds => 4,
-      :assists => 7,
-      :steals => 7,
-      :blocks => 15,
-      :slam_dunks => 10
+
+      "Babs" => {
+        :number => 3,
+        :shoe_size => 6,
+        :stats => {
+          :points => 22,
+          :rebounds => 13,
+          :assists => 8,
+          :steals => 10,
+          :blocks => 12,
+          :slam_dunks => 3
+        }
       },
-      {
-      :name => "DeSagna Diop", 
-      :number => 2,
-      :shoe => 14,
-      :points => 24,
-      :rebounds => 12,
-      :assists => 12,
-      :steals => 4,
-      :blocks => 5,
-      :slam_dunks => 5
+
+      "Taz" => {
+        :number => 5,
+        :shoe_size => 5,
+        :stats => {
+          :points => 28,
+          :rebounds => 7,
+          :assists => 15,
+          :steals => 4,
+          :blocks => 8,
+          :slam_dunks => 0
+        }
       },
-      {
-      :name => "Ben Gordon",
-      :number => 8,
-      :shoe => 15,
-      :points => 33,
-      :rebounds => 3,
-      :assists => 2,
-      :steals => 1,
-      :blocks => 1,
-      :slam_dunks => 0
+
+      "Daffy" => {
+        :number => 7,
+        :shoe_size => 15,
+        :stats => {
+          :points => 14,
+          :rebounds => 12,
+          :assists => 8,
+          :steals => 6,
+          :blocks => 10,
+          :slam_dunks => 3
+        }
       },
-      {
-      :name => "Brendan Haywood",
-      :number => 33,
-      :shoe => 15,
-      :points => 6,
-      :rebounds => 12,
-      :assists => 12,
-      :steals => 22,
-      :blocks => 5,
-      :slam_dunks => 12
-      }
-    ]
+
+      "Jordan" => {
+        :number => 23,
+        :shoe_size => 12,
+        :stats => {
+          :points => 40,
+          :rebounds => 8,
+          :assists => 4,
+          :steals => 12,
+          :blocks => 10,
+          :slam_dunks => 12
+        }
+      },
+    },
   }
 }
-end
-
-
-
