3ed802c391f433915aba34b274c519ed026d3c02
vibhu mahendru <vibhumahendru@gmail.com>
2018-10-08 23:11:42 +0000

    automatically backed up by learn

--- hashketball.rb
+++ hashketball.rb
@@ -33,27 +33,27 @@ def game_hash
       :assists => "10",
       :steals => "3",
       :blocks => "1",
-      :slam_dunks => "15"
+      :slam_dunks => 15
    }, {
      :player_name => "Mason Plumlee",
-      :number => "1",
-      :shoe => "19",
+      :number => 1,
+      :shoe => 19,
       :points => 26,
-      :rebounds => "12",
-      :assists => "6",
-      :steals => "3",
-      :blocks => "8",
-      :slam_dunks => "5"
+      :rebounds => 12,
+      :assists => 6,
+      :steals => 3,
+      :blocks => 8,
+      :slam_dunks => 5
    }, {
      :player_name => "Jason Terry",
-      :number => "31",
-      :shoe => "15",
+      :number => 31,
+      :shoe => 15,
       :points => 19,
-      :rebounds => "2",
-      :assists => "2",
-      :steals => "4",
-      :blocks => "11",
-      :slam_dunks => "1"
+      :rebounds => 2,
+      :assists => 2,
+      :steals => 4,
+      :blocks => 11,
+      :slam_dunks => 1
     }
    ]
  },
@@ -64,54 +64,54 @@ def game_hash
    :players => [
      {
        :player_name => "Jeff Adrien",
-      :number => "4",
-      :shoe => "18",
+      :number => 4,
+      :shoe => 18,
       :points => 10,
-      :rebounds => "1",
-      :assists => "1",
-      :steals => "2",
-      :blocks => "7",
-      :slam_dunks => "2"
+      :rebounds => 1,
+      :assists => 1,
+      :steals => 2,
+      :blocks => 7,
+      :slam_dunks => 2
      }, {
        :player_name => "Bismak Biyombo",
-      :number => "0",
-      :shoe => "16",
+      :number => 0,
+      :shoe => 16,
       :points => 12,
-      :rebounds => "4",
-      :assists => "7",
-      :steals => "7",
-      :blocks => "15",
-      :slam_dunks => "10"
+      :rebounds => 4,
+      :assists => 7,
+      :steals => 7,
+      :blocks => 15,
+      :slam_dunks => 10
      }, {
        :player_name => "DeSagna Diop",
-      :number => "2",
-      :shoe => "14",
+      :number => 2,
+      :shoe => 14,
       :points => 24,
-      :rebounds => "12",
-      :assists => "12",
-      :steals => "4",
-      :blocks => "5",
-      :slam_dunks => "5"
+      :rebounds => 12,
+      :assists => 12,
+      :steals => 4,
+      :blocks => 5,
+      :slam_dunks => 5
      }, {
        :player_name => "Ben Gordon",
-      :number => "8",
-      :shoe => "15",
+      :number =>8,
+      :shoe => 15,
       :points => 33,
-      :rebounds => "3",
-      :assists => "2",
-      :steals => "1",
-      :blocks => "1",
-      :slam_dunks => "0"
+      :rebounds => 3,
+      :assists => 2,
+      :steals => 1,
+      :blocks => 1,
+      :slam_dunks => 0
      }, {
        :player_name => "Brendan Haywood",
-      :number => "33",
-      :shoe => "15",
+      :number => 33,
+      :shoe => 15,
       :points => 6,
-      :rebounds => "12",
-      :assists => "12",
-      :steals => "22",
-      :blocks => "5",
-      :slam_dunks => "12"
+      :rebounds => 12,
+      :assists => 12,
+      :steals => 22,
+      :blocks => 5,
+      :slam_dunks => 12
      }
     ]
   }
@@ -144,6 +144,7 @@ def shoe_size(player)
   answer.to_i
 end
 
+def team_colors
 
 
 
